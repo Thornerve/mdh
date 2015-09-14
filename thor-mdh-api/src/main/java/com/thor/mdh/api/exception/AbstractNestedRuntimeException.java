5b73139aefce3d563402fc6acdbfe621557a249c
morlin <morlin@thor>
2015-09-14 22:37:42 +0800

    update

--- thor-mdh-api/src/main/java/com/thor/mdh/api/exception/AbstractNestedRuntimeException.java
+++ thor-mdh-api/src/main/java/com/thor/mdh/api/exception/AbstractNestedRuntimeException.java
@@ -86,7 +86,7 @@ public abstract class AbstractNestedRuntimeException extends RuntimeException {
      * @param exType the exception type to look for
      * @return whether there is a nested exception of the specified type
      */
-    public boolean contains(Class exType) {
+    public boolean contains(@SuppressWarnings("rawtypes") Class exType) {
         if (exType == null) {
             return false;
         }
--- thor-mdh-dao/src/main/java/com/thor/mdh/dao/Impl/account/AccountDao.java
+++ thor-mdh-dao/src/main/java/com/thor/mdh/dao/Impl/account/AccountDao.java
@@ -3,6 +3,7 @@ package com.thor.mdh.dao.Impl.account;
 import org.springframework.stereotype.Component;
 
 import com.thor.mdh.api.bean.LoginInfo;
+import com.thor.mdh.api.bean.UserBean;
 import com.thor.mdh.api.dao.IAccountDao;
 import com.thor.mdh.api.exception.TryNumLimitedException;
 import com.thor.mdh.api.exception.UserNotFoundException;
@@ -11,15 +12,29 @@ import com.thor.mdh.api.exception.UserNotFoundException;
 public class AccountDao implements IAccountDao{
 
 	@Override
-	public LoginInfo login(int appCode, String aliasName, String password) throws UserNotFoundException, TryNumLimitedException {
+	public LoginInfo userLogin(String aliasName, String password) throws UserNotFoundException, TryNumLimitedException {
 		
 		int i = 1;
 		if(1 == i){
+			/** 数据库查询是否存在该用户 */
 			throw new UserNotFoundException();
 		}else{
+			/** 登录错误次数是否达到上限 */
 			throw new TryNumLimitedException();
 		}
 		
+	}
+
+	@Override
+	public Boolean userInvalidate(Long userId) {
+		/** 讲数据状态置为2 */
+		return null;
+	}
+
+	@Override
+	public Long userRegister(UserBean userBean) {
+		/** 插入数据 */
+		return null;
 	}
 	
 }
