ebadb6a75475e5202f265263d380f5b01c54b514
morlin <morlin@thor>
2015-09-24 22:40:38 +0800

    update

--- thor-mdh-api/src/main/java/com/thor/mdh/api/bean/LoginInfo.java
+++ thor-mdh-api/src/main/java/com/thor/mdh/api/bean/LoginInfo.java
@@ -1,11 +1,14 @@
 package com.thor.mdh.api.bean;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class LoginInfo implements Serializable{
 
 	private static final long serialVersionUID = 6907602066738224986L;
 
+	private Long id;
+	
 	/** 返回代码,1 成功,2用户名或密码错误,3 当日失败次数限制 */
     private int rtnCode;
 
@@ -17,6 +20,9 @@ public class LoginInfo implements Serializable{
     
 	/** 登陆失败次数 */
     private int tryNum;
+    
+    /** 登录时间 */
+    private Date loginTime;
     
     public int getRtnCode() {
 		return rtnCode;
@@ -48,6 +54,22 @@ public class LoginInfo implements Serializable{
 
 	public void setTryNum(int tryNum) {
 		this.tryNum = tryNum;
+	}
+
+	public Date getLoginTime() {
+		return loginTime;
+	}
+
+	public void setLoginTime(Date loginTime) {
+		this.loginTime = loginTime;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 }
--- thor-mdh-dao/src/main/java/com/thor/mdh/dao/Impl/account/AccountDaoImpl.java
+++ thor-mdh-dao/src/main/java/com/thor/mdh/dao/Impl/account/AccountDaoImpl.java
@@ -11,6 +11,11 @@ import com.thor.mdh.api.bean.LoginInfo;
 import com.thor.mdh.api.bean.UserBean;
 import com.thor.mdh.api.dao.IAccountDao;
 
+/**
+ * 用户账户操作持久层
+ * @author morlin
+ *
+ */
 @Repository
 public class AccountDaoImpl implements IAccountDao{
 
@@ -19,37 +24,37 @@ public class AccountDaoImpl implements IAccountDao{
 	
 	@Override
 	public UserBean userLogin(Map<String,Object> paramMap) {
-		UserBean user = template.selectOne("", paramMap);
+		UserBean user = template.selectOne("com.thor.mdh.UserMapper.loginUser", paramMap);
 		return user;
 	}
 
 	@Override
 	public LoginInfo queryLoginTimes(Long userId) {
-		LoginInfo info = template.selectOne("", userId);
+		LoginInfo info = template.selectOne("com.thor.mdh.AccountMapper.loginUserTimes", userId);
 		return info;
 	}
 
 	@Override
 	public Long insertLoginLog(LoginInfo info) {
-		int id = template.insert("", info);
+		int id = template.insert("com.thor.mdh.AccountMapper.insertLoginLog", info);
 		return (long) id;
 	}
 
 	@Override
 	public String queryEmailById(Long userId) {
-		// TODO Auto-generated method stub
-		return null;
+		String email = template.selectOne("com.thor.mdh.UserMapper.queryEmailByUserId", userId);
+		return email;
 	}
 
 	@Override
 	public String queryMobileById(Long userId) {
-		// TODO Auto-generated method stub
-		return null;
+		String mobile = template.selectOne("com.thor.mdh.UserMapper.queryMobileByUserId", userId);
+		return mobile;
 	}
 
 	@Override
 	public Long updateUserPassWord(String password, Long userId) {
-		// TODO Auto-generated method stub
-		return null;
+		int id = template.update("com.thor.mdh.UserMapper.updatePassWordById", userId);
+		return (long) id;
 	}
 }
